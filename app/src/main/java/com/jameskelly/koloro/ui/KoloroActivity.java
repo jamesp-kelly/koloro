5253b239aaa7b6358bbadf22843963acfb8757a7
James Kelly <jk_jameskelly@outlook.com>
2016-05-16 17:49:04 -0400

    start home preference anim

--- app/src/main/java/com/jameskelly/koloro/ui/KoloroActivity.java
+++ app/src/main/java/com/jameskelly/koloro/ui/KoloroActivity.java
@@ -1,18 +1,23 @@
 package com.jameskelly.koloro.ui;
 
+import android.animation.Animator;
 import android.app.Activity;
 import android.content.Intent;
 import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.Switch;
+import android.widget.ImageButton;
+import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
-import butterknife.OnItemSelected;
 import com.jameskelly.koloro.KoloroApplication;
 import com.jameskelly.koloro.R;
 import com.jameskelly.koloro.preferences.BooleanPreference;
@@ -20,9 +25,8 @@ import com.jameskelly.koloro.preferences.IntPreference;
 import com.jameskelly.koloro.preferences.PreferencesModule;
 import com.jameskelly.koloro.preferences.SimpleSpinnerAdapter;
 import com.jameskelly.koloro.service.KoloroService;
+import com.jameskelly.koloro.ui.layouts.AnimatorEndListener;
 import com.jameskelly.koloro.ui.views.KoloroView;
-import java.util.Arrays;
-import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Named;
 
@@ -44,11 +48,16 @@ public class KoloroActivity extends BaseActivity implements KoloroView {
   IntPreference captureButtonPositionPreference;
   @Inject MediaProjectionManager mediaProjectionManager;
 
-  @BindView(R.id.switch_show_notification) Switch showNotificationSwitch;
-  @BindView(R.id.switch_capture_button_visible) Switch captureButtonVisibleSwitch;
-  @BindView(R.id.switch_store_captures) Switch storeCapturesSwitch;
-  @BindView(R.id.switch_multi_shot) Switch multiShotSwitch;
-  @BindView(R.id.spinner_capture_button_position) Spinner captureButtonPositionSpinner;
+  @BindDimen(R.dimen.prefs_layout_margin_top) int prefsLayoutMarginTop;
+
+  @BindView(R.id.prefs_button) ImageButton prefsButton;
+  @BindView(R.id.prefs_layout) ViewGroup prefsLayout;
+
+  //@BindView(R.id.switch_show_notification) Switch showNotificationSwitch;
+  //@BindView(R.id.switch_capture_button_visible) Switch captureButtonVisibleSwitch;
+  //@BindView(R.id.switch_store_captures) Switch storeCapturesSwitch;
+  //@BindView(R.id.switch_multi_shot) Switch multiShotSwitch;
+  //@BindView(R.id.spinner_capture_button_position) Spinner captureButtonPositionSpinner;
   @BindView(R.id.start_capture) Button startCaptureButton;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -57,78 +66,120 @@ public class KoloroActivity extends BaseActivity implements KoloroView {
     KoloroApplication.get(this).applicationComponent().inject(this);
     ButterKnife.bind(this);
 
+    prefsLayout.setEnabled(false);
+
     bindSharedPreferences();
   }
 
   @Override public void bindSharedPreferences() {
-    showNotificationSwitch.setChecked(showNotificationPreference.get());
-    captureButtonVisibleSwitch.setChecked(captureButtonVisiblePreference.get());
-    storeCapturesSwitch.setChecked(storeCapturesInGalleryPreference.get());
-    multiShotSwitch.setChecked(multiShotPreference.get());
+    //showNotificationSwitch.setChecked(showNotificationPreference.get());
+    //captureButtonVisibleSwitch.setChecked(captureButtonVisiblePreference.get());
+    //storeCapturesSwitch.setChecked(storeCapturesInGalleryPreference.get());
+    //multiShotSwitch.setChecked(multiShotPreference.get());
 
-    captureButtonPositionAdapter = new SimpleSpinnerAdapter(this, buttonPositionValues());
-    captureButtonPositionSpinner.setAdapter(captureButtonPositionAdapter);
-    captureButtonPositionSpinner.setSelection(captureButtonPositionPreference.get());
+    //captureButtonPositionAdapter = new SimpleSpinnerAdapter(this, buttonPositionValues());
+    //captureButtonPositionSpinner.setAdapter(captureButtonPositionAdapter);
+    //captureButtonPositionSpinner.setSelection(captureButtonPositionPreference.get());
   }
 
-  private List<String> buttonPositionValues() {
-    String[] stringArray = getResources().getStringArray(R.array.capture_button_position_array);
-    return Arrays.asList(stringArray);
-  }
+  //private List<String> buttonPositionValues() {
+  //  String[] stringArray = getResources().getStringArray(R.array.capture_button_position_array);
+  //  return Arrays.asList(stringArray);
+  //}
 
-  @OnCheckedChanged(R.id.switch_show_notification)
-  void onShowNotificationChanged() {
-    boolean newValue = showNotificationSwitch.isChecked();
-    boolean oldValue = showNotificationPreference.get();
+  //@OnCheckedChanged(R.id.switch_show_notification)
+  //void onShowNotificationChanged() {
+  //  boolean newValue = showNotificationSwitch.isChecked();
+  //  boolean oldValue = showNotificationPreference.get();
+  //
+  //  if (newValue != oldValue) {
+  //    Log.d(TAG, String.format("Updating 'show notification' preference to %s", newValue));
+  //    showNotificationPreference.set(newValue);
+  //  }
+  //}
+  //
+  //@OnCheckedChanged(R.id.switch_capture_button_visible)
+  //void onCaptureButtonChanged() {
+  //  boolean newValue = captureButtonVisibleSwitch.isChecked();
+  //  boolean oldValue = captureButtonVisiblePreference.get();
+  //
+  //  if (newValue != oldValue) {
+  //    Log.d(TAG, String.format("Updating 'capture button visible' preference to %s", newValue));
+  //    captureButtonVisiblePreference.set(newValue);
+  //  }
+  //}
+  //
+  //@OnCheckedChanged(R.id.switch_store_captures)
+  //void onStoreCapturesChanged() {
+  //  boolean newValue = storeCapturesSwitch.isChecked();
+  //  boolean oldValue = storeCapturesInGalleryPreference.get();
+  //
+  //  if (newValue != oldValue) {
+  //    Log.d(TAG, String.format("Updating 'store captures' preference to %s", newValue));
+  //    storeCapturesInGalleryPreference.set(newValue);
+  //  }
+  //}
+  //
+  //@OnCheckedChanged(R.id.switch_multi_shot)
+  //void onMultiShotChanged() {
+  //  boolean newValue = multiShotSwitch.isChecked();
+  //  boolean oldValue = multiShotPreference.get();
+  //
+  //  if (newValue != oldValue) {
+  //    Log.d(TAG, String.format("Updating 'store captures' preference to %s", newValue));
+  //    multiShotPreference.set(newValue);
+  //  }
+  //}
+  //
+  //@OnItemSelected(R.id.spinner_capture_button_position)
+  //void onCaptureMethodChanged(int position) {
+  //  int newValue = position;
+  //  int oldValue = captureButtonPositionPreference.get();
+  //
+  //  if (newValue != oldValue) {
+  //    Log.d(TAG, String.format("Updating 'capture button position' preference to %s", newValue));
+  //    captureButtonPositionPreference.set(newValue);
+  //  }
+  //}
 
-    if (newValue != oldValue) {
-      Log.d(TAG, String.format("Updating 'show notification' preference to %s", newValue));
-      showNotificationPreference.set(newValue);
+  @OnClick(R.id.prefs_button)
+  void onPrefsClicked(View v) {
+    if (prefsLayout.getVisibility() == View.INVISIBLE) {
+      prefsButton.animate()
+          .translationYBy(prefsButton.getHeight() + prefsLayoutMarginTop)
+          .setDuration(150)
+          .setInterpolator(new AccelerateInterpolator(2.0f))
+          .setListener(new AnimatorEndListener() {
+            @Override public void onAnimationEnd(Animator animation) {
+              prefsButton.setImageDrawable(null);
+              layoutExpanAnimation();
+            }
+          });
+    } else {
+
+      prefsLayout.setVisibility(View.INVISIBLE); //animation needed
+
+      prefsButton.animate()
+          .translationYBy((prefsButton.getHeight() + prefsLayoutMarginTop) * -1)
+          .setDuration(150)
+          .setInterpolator(new DecelerateInterpolator(2.0f))
+          .setListener(new AnimatorEndListener() {
+            @Override public void onAnimationEnd(Animator animation) {
+              prefsButton.setImageDrawable(getResources().getDrawable(R.drawable.ic_colorize_white_24dp, null));
+            }
+          });
     }
   }
 
-  @OnCheckedChanged(R.id.switch_capture_button_visible)
-  void onCaptureButtonChanged() {
-    boolean newValue = captureButtonVisibleSwitch.isChecked();
-    boolean oldValue = captureButtonVisiblePreference.get();
+  private void layoutExpanAnimation() {
+    int x = prefsLayout.getWidth();
+    int y = 0;
+    float maxRadius = (float) Math.hypot(prefsLayout.getWidth(), prefsLayout.getHeight());
 
-    if (newValue != oldValue) {
-      Log.d(TAG, String.format("Updating 'capture button visible' preference to %s", newValue));
-      captureButtonVisiblePreference.set(newValue);
-    }
-  }
-
-  @OnCheckedChanged(R.id.switch_store_captures)
-  void onStoreCapturesChanged() {
-    boolean newValue = storeCapturesSwitch.isChecked();
-    boolean oldValue = storeCapturesInGalleryPreference.get();
-
-    if (newValue != oldValue) {
-      Log.d(TAG, String.format("Updating 'store captures' preference to %s", newValue));
-      storeCapturesInGalleryPreference.set(newValue);
-    }
-  }
-
-  @OnCheckedChanged(R.id.switch_multi_shot)
-  void onMultiShotChanged() {
-    boolean newValue = multiShotSwitch.isChecked();
-    boolean oldValue = multiShotPreference.get();
-
-    if (newValue != oldValue) {
-      Log.d(TAG, String.format("Updating 'store captures' preference to %s", newValue));
-      multiShotPreference.set(newValue);
-    }
-  }
-
-  @OnItemSelected(R.id.spinner_capture_button_position)
-  void onCaptureMethodChanged(int position) {
-    int newValue = position;
-    int oldValue = captureButtonPositionPreference.get();
-
-    if (newValue != oldValue) {
-      Log.d(TAG, String.format("Updating 'capture button position' preference to %s", newValue));
-      captureButtonPositionPreference.set(newValue);
-    }
+    Animator animator = ViewAnimationUtils.createCircularReveal(prefsLayout, x, y, 200, maxRadius);
+    prefsLayout.setVisibility(View.VISIBLE);
+    animator.setInterpolator(new AccelerateDecelerateInterpolator());
+    animator.start();
   }
 
   @OnClick(R.id.start_capture)
